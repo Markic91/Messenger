9ba22a1a5a6123add3fe5fc27c103c4400970ac0
ANTIC MARKO <[antic.marko69@gmail.com](mailto:antic.marko69@gmail.com)>
2023-09-05 15:49:59 +0200

    coucou

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args){
     }
 }
